ad487ae7af7a9d84f2faef4a7779745ae8c220ff
TranLePhat <macnhon2020@gmail.com>
2026-06-21 10:23:48 +0000

    add Disk check

--- Project/Server-Performance-Stats/server-stats.sh
+++ Project/Server-Performance-Stats/server-stats.sh
@@ -3,11 +3,18 @@
 
 echo "SERVER PERFORMANCE REPORT"
 echo "=============================="
-
+#CPU
 echo "CPU USAGE"
 
 top -bn1 | grep "Cpu(s)"
+#RAM
 echo
 echo "=============================="
 echo "MEMORY USAGE"
 free -h
+#DISK
+echo
+echo "DISK USAGE"
+
+df -h /
+#NETWORK
